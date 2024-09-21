92924a26e4bd7cf87983b88ae40b9f744619ba21
tibetyo <zjhj0814@gamil.com>
2024-09-21 23:29:21 +0900

    last commit

--- src/main/java/study/datajpa/entity/BaseEntity.java
+++ src/main/java/study/datajpa/entity/BaseEntity.java
@@ -1,16 +1,14 @@
 package study.datajpa.entity;
 
 import jakarta.persistence.Column;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 
 public class BaseEntity extends BaseTimeEntity{
-    @CreatedDate
+    @CreatedBy
     @Column(updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
+    private String createdBy;
+    @LastModifiedBy
+    private String lastModifiedBy;
 
 }
--- src/main/java/study/datajpa/entity/BaseTimeEntity.java
+++ src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -5,16 +5,20 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseTimeEntity {
-    @CreatedBy
+    @CreatedDate
     @Column(updatable = false)
-    private String createdBy;
-    @LastModifiedBy
-    private String lastModifiedBy;
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
